652b943af20af1252c86a68d6f12ed7076f8bd50
Gabriel Carrizales <102924032+Gabriel220702@users.noreply.github.com>
2025-03-30 20:30:25 -0600

    Add files via upload

---
+++ Innova TecNM 2024 Etapa Regional/Página Web/SignIn_SignUp/config.php
@@ -0,0 +1,5 @@
+<?php
+    $conx = mysqli_connect("localhost","u160637135_Gabriel_2207","Amoxtli-Jap,2024,INNOVATECNM","u160637135_AmoxtliJap");
+    if(!$conx){
+        echo 'Connection Failed';
+    }
---
+++ Innova TecNM 2024 Etapa Regional/Página Web/php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+    $conexion = mysqli_connect("localhost","u160637135_Gabriel_2207","Amoxtli-Jap,2024,INNOVATECNM","u160637135_AmoxtliJap");
+
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+    */
